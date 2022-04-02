6edaf9b6c583ebba80eb8d9c1307de1cd0b794a9
Winnie Pérez Martínez <71612467+winnieepm@users.noreply.github.com>
2022-04-02 02:03:19 -0400

    Delete pages directory

--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-   <div>
-      <NuxtLink to="/"><h1>Home</h1></NuxtLink>
-      
-      <p>This is just a paragraph to make sure that everything is working out here~</p>
-      <br>
-      <h3><NuxtLink to="/first">First</NuxtLink></h3>
-    </div>
-</template>
