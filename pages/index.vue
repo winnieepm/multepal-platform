7fb8171fe6097070a9990e62fac1c8b5a81ccff4
donttrust <71612467+winnieepm@users.noreply.github.com>
2022-05-12 18:02:39 -0400

    content update

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
       
             <div class="body" style="background-color: rgba(124, 113, 227, 0.70);">
                 <p class="after-heading">
-                  This is a digital field journal that explores creative approaches for representing and understanding the construction of space-time in Luis Othoniel Rosa's <i>Caja de fractales</i> (2017). It asks, "What can space-time look like?" to explore the nuances in the experience of being inside of time as a phenomenon has for Caribbean people, and how its geographic specificity influences said experience.
+                  This is a digital field journal that explores creative and digital approaches for representing and understanding the construction of space-time in Luis Othoniel Rosa's <i>Caja de fractales</i> (2017). It asks, "What can space-time look like?" hoping to uncover some nuances in the experience of being inside of time as a phenomenon that are specific to Caribbean people, and how its geographic specificity influences said experience.
                 </p>
                 <p>
                   <strong>Novel Summary</strong> <br/>
@@ -20,7 +20,7 @@
                 </p>
               <h4><strong>• Attempts •</strong></h4>
               <!-- #00 -->
-                <p>
+                <p id="a-00">
                   <b>#00 -</b> (2022-05-12) | Connecting the timeline. From the outermost part of the text's infrastructure, the first markers of time are the chapters' numbering and the year they mark, but their textual sequence does not align with their chronological order. 
                 </p>
               
@@ -30,13 +30,59 @@
                 What would it look like to connect the narrative's years chronologically? By altering the years' order, Rosa hints at the possibilities for play in the understanding of time as he subtly inserts the possibility that time isn't only read linearly. <br/>The novel thus argues for a specific ordering of events while accepting its potential for multiple temporal orderings.
               </p>
               <p class="note">
-                [Note: See Cabiya's <i>Malas hierbas</i>() and Julio Cortázar's <i>Rayuela</i> () for similar index strategies.]
+                [Note: See Cabiya's <i>Malas hierbas</i> (2011) and Julio Cortázar's <i>Rayuela</i> (1963) for similar index strategies.]
               </p>
               <p>
-                In a series of hand drawings, I followed the novel's sequential order of chapters and attempted to connect their respective years chronologically. 
+                In a series of hand drawings, I followed the novel's sequential order of chapters and attempted to connect their respective years chronologically. Here are those drawings:
               </p>
-          </div>
-    </div>
+              
+              <div class="row">
+                  <div class="col-sm-12 col-md-7">
+                      <figure class="figure">
+                          <img src="~/assets/space-time/rosa-sketch-01.png" class="figure-img img-fluid">
+                          <figcaption class="figure-caption">Figure 1. Using curved, interlaced lines to represent how the narrative jumps across the chapters' timeline. Years are horizontally aligned.</figcaption>
+                      </figure>
+                  </div>
+
+                  <div class="col-sm-12 col-md-5">
+                        <figure class="figure">
+                            <img src="~/assets/space-time/rosa-sketch-02.png" class="figure-fluid img-fluid">
+                            <figcaption class="figure-caption">Figure 2. Using curved lines to represent how the narrative jumps across the chapters' timeline. Years are vertically aligned.</figcaption>
+                        </figure>
+                  </div>
+              </div>
+              <p>
+                For this step, I used curved lines to represent the idea of jumps, as in jumping through time. The vertical vs. horizontal timeline alignment intended to change the angle of perspective and probe the stability of the twisted form that surges from the years' connection. <br/> In <i>Figure 1.</i>, I opted for lines that transcended or trespassed the year number entirely whereas in <i>Figure 2.</i> the lines bounce off the same invisible vertical axis formed by the years without crossing it. <br/> There is no logical reasoning guiding my decision to orient the curved lines differently for each drawing. Rather, I am merely guided by the curiosity of finding a new shape that makes sense for the narrrarive timeline. What is a shape that makes sense? One that elicits new insights for my understanding of the text. 
+              </p>
+              <p>
+                Insights: From this drawings I identify the urge to suggest that the novel is a closed narrative circle because its earliest event occurs in 2018 and its last chapter returns to this very moment. However, 2018 does not mark the narrative beginning of <i>Caja de fractales</i>, but 2028 does. In this scenario, the return to 2018 flags it as an important narrative instance but not inherently its beginning. Its import prevents it from ocuppying the position of origin, at least as the text initially indicates. 
+              </p>
+
+              <p id="a-01" class="after-heading">
+                <b>#01 -</b> (2022-05-10) | Composing a simple graph. I used a two-dimension graph to produce a different image from the aligned years. The X axis is the chronological novel timeline wih its Y axis grouping the chapter's sequential order. 
+              </p>
+
+              <div class="row">
+                  <div class="col-sm-12 col-md-11 mx-auto" style="width: 60%;">
+                      <figure class="figure">
+                          <img src="~/assets/space-time/rosa-sketch-03.png" class="figure-img img-fluid">
+                          <figcaption class="figure-caption">Figure 3. Narrative temporal movement graph.</figcaption>
+                      </figure>
+                  </div>
+
+                  <div class="col-sm-12 col-md-10 mx-auto" style="width: 70%;">
+                        <figure class="figure">
+                            <img src="~/assets/space-time/rosa-sketch-04.png" class="figure-fluid img-fluid">
+                            <figcaption class="figure-caption">Figure 4. Using curved lines to represent how the narrative jumps across the chapters' timeline. Years are vertically aligned.</figcaption>
+                        </figure>
+                  </div>
+              </div>
+
+              <p id="a-02" class="after-heading">
+                  <b>#02 -</b> (2022-05-12) | Listing textual objects that are meaningful for the novel's space-time construction.
+              </p>
+            </div>
+      </div>
   </div>
 </template>
 
