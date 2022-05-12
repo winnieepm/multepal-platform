7fdd9dde4c7508e091fc32778788d64c87166c15
donttrust <71612467+winnieepm@users.noreply.github.com>
2022-05-12 11:53:21 -0400

    content update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,24 @@
 export default {
+  
   css: [
     '~/assets/main.css',
     '~/assets/fonts/play.css'
   ],
 
-  script: [
-        {
-          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'
-        },
-      ],
+  head: {
 
-  link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Play&display=swap'
+      link: [
+            {
+              rel: 'stylesheet', type: 'text/css',
+              href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
+            }
+          ],
+
+      script: [
+            {
+              src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js',
+              src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js'
+            },
+          ]
         }
-      ],
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,42 @@
 <template>
-  <div id="home">
+  <!-- <div id="home">
       <navbar/>
       <h1>Building Blog</h1>
       <h3>By Winnie</h3>
+  </div> -->
+  <div id="hamilton-final">
+    <h1>Notes on Space-Time in Caja de fractales</h1>
+    <h4>By Winnie Pérez Martínez</h4>
+
+    <div class="container">
+      
+            <div class="body" style="background-color: rgba(124, 113, 227, 0.70);">
+                <p class="after-heading">
+                  This is a digital field journal that explores creative approaches for representing and understanding the construction of space-time in Luis Othoniel Rosa's <i>Caja de fractales</i> (2017). It asks, "What can space-time look like?" to explore the nuances in the experience of being inside of time as a phenomenon has for Caribbean people, and how its geographic specificity influences said experience.
+                </p>
+                <p>
+                  <strong>Novel Summary</strong> <br/>
+                  <!-- ADD TEXT SUMMARY -->
+                </p>
+              <h4><strong>• Attempts •</strong></h4>
+              <!-- #00 -->
+                <p>
+                  <b>#00 -</b> (2022-05-12) | Connecting the timeline. From the outermost part of the text's infrastructure, the first markers of time are the chapters' numbering and the year they mark, but their textual sequence does not align with their chronological order. 
+                </p>
+              
+              <img src="~/assets/space-time/rosa-indice.jpg" style="height: auto; width: 30%;"> <br/>
+
+              <p>
+                What would it look like to connect the narrative's years chronologically? By altering the years' order, Rosa hints at the possibilities for play in the understanding of time as he subtly inserts the possibility that time isn't only read linearly. <br/>The novel thus argues for a specific ordering of events while accepting its potential for multiple temporal orderings.
+              </p>
+              <p class="note">
+                [Note: See Cabiya's <i>Malas hierbas</i>() and Julio Cortázar's <i>Rayuela</i> () for similar index strategies.]
+              </p>
+              <p>
+                In a series of hand drawings, I followed the novel's sequential order of chapters and attempted to connect their respective years chronologically. 
+              </p>
+          </div>
+    </div>
   </div>
 </template>
 
